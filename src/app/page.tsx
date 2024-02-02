2b165d500a473672838b42b96a800ec4146dfe60
Anthoni Campos <anthonithomes@gmail.com>
2024-02-01 21:21:33 -0300

    teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-'use client';
+"use client";
 
-import { Flex, Text } from '@chakra-ui/react';
-
+import { Flex, Text, Progress } from "@chakra-ui/react";
 export default function Home() {
+
+  const teste = () => {
+    window.alert("Olá mundo")
+  }
+
   return (
-    <Flex h="100vh" justifyContent="center" alignItems="center">
+    <Flex h="100vh" justifyContent="center" alignItems="center" flexDir={"column"}>
       <Text>Este é o template de React para CT Junior, criado em 2023</Text>
     </Flex>
   );
