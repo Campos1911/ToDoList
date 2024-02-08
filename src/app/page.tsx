8e5bf327405d095200cfdc304a213ffb969c4477
Anthoni Campos <anthonithomes@gmail.com>
2024-02-08 18:07:57 -0300

    testando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Input, Button, Flex, Text } from "@chakra-ui/react";
+import { Input, Button, Flex, Text, Select } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 
 import Todo from "../components/TodoList/todo";
@@ -29,19 +29,28 @@ export default function Home() {
   });
 
   return (
-    <Flex flexDir="column" alignItems="center" padding="30px" marginTop="14vh">
-      <Text
-        fontSize="36px"
-        fontWeight="bold"
-        color="white"
-        marginRight="28vw"
-        marginBottom="5vh"
-      >
-        T O D O
-      </Text>
+    <Flex flexDir="column" alignItems="center" padding="10px" marginTop="14vh">
+      <Flex flexDir="row">
+        <Text
+          marginRight="14vw"
+          fontSize="36px"
+          fontWeight="bold"
+          color="white"
+          marginBottom="9vh"
+        >
+          T O D O
+        </Text>
+        <Flex marginTop="1vh">
+          <Select color="black" borderColor="black" _hover={{ borderColor: "rgb(27, 27, 50)"}}  focusBorderColor="null">
+            <option value="">Escolha um filtro</option>
+            <option value="done">Concluídas</option>
+            <option value="awaiting">Não concluídas</option>
+          </Select>
+        </Flex>
+      </Flex>
       <Todoform />
       <div className="todo-list">
-      {dados.map((todo:dadosProps) => (
+        {dados.map((todo: dadosProps) => (
           <Todo key={todo.id} todo={todo} />
         ))}
       </div>
